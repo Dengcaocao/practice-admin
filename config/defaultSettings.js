bdcfa6e2ba28b207a46a100d3ad38b86de42f4da
邓威 <1536698087@qq.com>
2021-11-10 22:27:56 +0800

    项目初始化、去除多余的代码。重写全局lodading页、headRightContent、login页重写布局、清理菜单

---
+++ config/defaultSettings.js
@@ -0,0 +1,15 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '小草后台',
+  pwa: false,
+  logo: '/logo-bg.png',
+  iconfontUrl: '',
+};
+export default Settings;
---
+++ config/routes.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    path: '/login',
+    name: 'login',
+    component: './Login',
+    layout: false,
+    hideInMenu: true
+  },
+  {
+    path: '/'
+  },
+  {
+    component: './404',
+  },
+];
