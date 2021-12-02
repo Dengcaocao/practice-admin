80b19f2bebc36bbd010bf4117aefb965dee6db84
dw <1536698087@qq.com>
2021-12-02 11:11:07 +0800

    🌷 UI(商品管理): 商品信息弹窗新增分类、封面图

--- src/pages/Goods/infoModal.jsx
+++ src/pages/Goods/infoModal.jsx
@@ -1,11 +1,22 @@
-import { Modal, Form, message, InputNumber } from 'antd';
+import { Modal, Form, message, Cascader, Upload } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import ProForm, { ProFormText, ProFormMoney, ProFormTextArea } from '@ant-design/pro-form';
 import { useState, forwardRef, useImperativeHandle } from 'react';
 
 export default forwardRef((props, ref) => {
   const { actionTable } = props;
+  const [goodsForm] = Form.useForm();
   const [modalType, setModalType] = useState('');
   const [isInfoModal, setIsInfoModal] = useState(false);
+  const [uploadState, setUploadState] = useState(false);
+  const [imageUrl, setImageUrl] = useState('');
+
+  const uploadButton = (
+    <div>
+      {uploadState ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
   useImperativeHandle(ref, () => ({
     setIsInfoModal: (goodsId) => {
@@ -13,6 +24,45 @@ export default forwardRef((props, ref) => {
       setIsInfoModal(true);
     },
   }));
+
+  const options = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang',
+      children: [
+        {
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [
+            {
+              value: 'xihu',
+              label: 'West Lake',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  const onChange = (value) => {
+    console.log(value);
+  };
 
   /**
    * 处理添加商品
@@ -23,11 +73,56 @@ export default forwardRef((props, ref) => {
   };
 
   /**
+   * 获取商品详情
+   */
+  const handleDetail = async () => {
+    // 获取表单字段
+    console.log(goodsForm.getFieldsValue(true));
+  };
+
+  /**
    * 处理更新商品
    * @param {*} values
    */
   const handleUpdate = async () => {
     console.log('更新商品');
+  };
+
+  /**
+   * 封面图处理
+   * @param {*} values
+   */
+  function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  };
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      return setUploadState(true);
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (imageUrl) => {
+        setUploadState(false);
+        setImageUrl(imageUrl);
+      });
+    }
+    if (info.file.status === 'error') {
+      setUploadState(false);
+    }
   };
 
   const handleSubmit = async (values) => {
@@ -47,15 +142,20 @@ export default forwardRef((props, ref) => {
       onCancel={() => setIsInfoModal(false)}
     >
       <ProForm
+        form={goodsForm}
         className="goods-form"
         preserve={false}
         onFinish={async (values) => {
           handleSubmit(values);
         }}
-        initialValues={{
-          name: '蚂蚁设计有限公司',
-        }}
       >
+        <ProForm.Item
+          name="category"
+          label="分类"
+          rules={[{ required: true, message: '请选择分类' }]}
+        >
+          <Cascader options={options} onChange={onChange} placeholder="Please select" />
+        </ProForm.Item>
         <ProFormText
           name="title"
           label="标题"
@@ -74,6 +174,25 @@ export default forwardRef((props, ref) => {
           rules={[{ required: true, message: '请输入库存' }]}
           min={0}
         />
+        <ProForm.Item
+          name="img"
+          label="封面图"
+          rules={[{ required: true, message: '请选择封面图' }]}
+        >
+          <Upload
+            listType="picture-card"
+            showUploadList={false}
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+          >
+            {imageUrl ? (
+              <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
+        </ProForm.Item>
         <ProFormTextArea
           label="描述"
           name="description"
